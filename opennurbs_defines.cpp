f4142ab7dd07b86aa430f1cb0098b06f488d903d
Pei Jia <jiapei@longervision.com>
2019-06-09 15:28:28 -0700

    Add concern for Linux

--- opennurbs_defines.cpp
+++ opennurbs_defines.cpp
@@ -503,7 +503,6 @@ int ON::CloseAllFiles()
   //fcloseall is not supported on OS X
   return EOF;
 #elif defined(ON_COMPILER_GNU)
-//#error TODO - call gcc fcloseall()
   return fcloseall(); 
 #else
   // I can't find an fcloseall() or _fcloseall() in
@@ -556,6 +555,7 @@ ON::RuntimeEnvironment ON::RuntimeEnvironmentFromUnsigned(
   ON_ENUM_FROM_UNSIGNED_CASE(ON::RuntimeEnvironment::Windows);
   ON_ENUM_FROM_UNSIGNED_CASE(ON::RuntimeEnvironment::Apple);
   ON_ENUM_FROM_UNSIGNED_CASE(ON::RuntimeEnvironment::Android);
+  ON_ENUM_FROM_UNSIGNED_CASE(ON::RuntimeEnvironment::Linux);
   }
   ON_ERROR("Invalid runtime_environment_as_unsigned parameter value.");
   return (ON::RuntimeEnvironment::Unset);
@@ -569,6 +569,8 @@ ON::RuntimeEnvironment ON::CurrentRuntimeEnvironment()
   return ON::RuntimeEnvironment::Apple;
 #elif defined (ON_RUNTIME_ANDROID)
   return ON::RuntimeEnvironment::Android;
+#elif defined (ON_RUNTIME_LINUX)
+  return ON::RuntimeEnvironment::Linux;
 #else
   ON_ERROR("ON_RUNTIME_... not defined.");
   return ON::RuntimeEnvironment::Unset;
