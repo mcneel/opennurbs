6042082827254b5d270dd2c81654a8125a8ed698
Bozo the Builder <bozo@mcneel.com>
2023-12-13 03:39:15 -0800

    Sync changes from upstream repository
    Co-authored-by: Dale Lear <dalelear@mcneel.com>

--- opennurbs_public_version.h
+++ opennurbs_public_version.h
@@ -6,7 +6,7 @@
 
 // To update version numbers, edit ..\build\build_dates.msbuild
 #define RMA_VERSION_MAJOR 8
-#define RMA_VERSION_MINOR 3
+#define RMA_VERSION_MINOR 4
 
 ////////////////////////////////////////////////////////////////
 //
@@ -15,9 +15,9 @@
 //
 #define RMA_VERSION_YEAR   2023
 #define RMA_VERSION_MONTH  12
-#define RMA_VERSION_DATE   12
+#define RMA_VERSION_DATE   13
 #define RMA_VERSION_HOUR   3
-#define RMA_VERSION_MINUTE 5
+#define RMA_VERSION_MINUTE 27
 
 ////////////////////////////////////////////////////////////////
 //
@@ -35,8 +35,8 @@
 //     3 = build system release build
 #define RMA_VERSION_BRANCH 0
 
-#define VERSION_WITH_COMMAS  8,3,23346,3050
-#define VERSION_WITH_PERIODS 8.3.23346.03050
+#define VERSION_WITH_COMMAS  8,4,23347,3270
+#define VERSION_WITH_PERIODS 8.4.23347.03270
 #define COPYRIGHT "Copyright (C) 1993-2023, Robert McNeel & Associates. All Rights Reserved."
 #define SPECIAL_BUILD_DESCRIPTION "Public OpenNURBS C++ 3dm file IO library."
 
@@ -44,11 +44,11 @@
 #define RMA_VERSION_NUMBER_MAJOR_WSTRING L"8"
 #define RMA_PREVIOUS_VERSION_NUMBER_MAJOR_WSTRING L"7"
 
-#define RMA_VERSION_NUMBER_SR_STRING   "SR3"
-#define RMA_VERSION_NUMBER_SR_WSTRING L"SR3"
+#define RMA_VERSION_NUMBER_SR_STRING   "SR4"
+#define RMA_VERSION_NUMBER_SR_WSTRING L"SR4"
 
-#define RMA_VERSION_WITH_PERIODS_STRING   "8.3.23346.03050"
-#define RMA_VERSION_WITH_PERIODS_WSTRING L"8.3.23346.03050"
+#define RMA_VERSION_WITH_PERIODS_STRING   "8.4.23347.03270"
+#define RMA_VERSION_WITH_PERIODS_WSTRING L"8.4.23347.03270"
 
 
 
